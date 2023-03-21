9ea5de1280da2af81a124da4818359d890391bdf
Mekuzd <emekaseun.es@gmail.com>
2023-03-21 14:04:06 +0100

    created error 404 middleware and established db connection

--- API/index.js
+++ API/index.js
@@ -1,3 +1,31 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+const { PORT, MONGO_URL } = require("./src/Config/config");
+const Error404 = require("./src/middleware/error404");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("welcome to home");
+});
+
+app.use(Error404); // unavailable route
+
+// connect to db and listen to port
+const start = async () => {
+  try {
+    await mongoose.connect(MONGO_URL);
+    app.listen(PORT, () =>
+      console.log(`Server is listening on port ${PORT}...`),
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- API/src/Config/config.js
+++ API/src/Config/config.js
@@ -1,1 +1,4 @@
 require("dotenv").config();
+const { MONGO_URL, PORT } = process.env;
+
+module.exports = { MONGO_URL, PORT };
