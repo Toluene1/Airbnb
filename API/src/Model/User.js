ee7b1cdfb621b009c04e6d5486db137420cc1a1f
Toluene1 <adaramoti111@gmail.com>
2023-03-22 22:20:33 +0100

    added the User Schema

---
+++ API/src/Model/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    FirstName: {
+      type: Schema.Types.String,
+      required: true,
+    },
+    LastName: {
+      type: Schema.Types.String,
+      required: true,
+    },
+    Email: {
+      type: Schema.Types.String,
+      required: true,
+      unique: true,
+    },
+    DOB: {
+      type: Schema.Types.String,
+      required: true,
+    },
+    Avatar: {
+      type: Schema.Types.String,
+      default: "",
+    },
+    PhoneNumber: {
+      type: Schema.Types.String,
+      required: true,
+    },
+    Address: {
+      type: Schema.Types.String,
+      default: "",
+    },
+
+    EmergencyContact: {
+      type: Schema.Types.String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model("users", userSchema);
+
+module.exports = User;
