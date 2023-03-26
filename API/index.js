1c8b0de005db200129ade7375810eb17055f4704
Toluene1 <adaramoti111@gmail.com>
2023-03-26 22:51:18 +0100

    added the get Request for User Details

--- API/index.js
+++ API/index.js
@@ -6,6 +6,7 @@ const { PORT, MONGO_URL } = require("./src/Config/config");
 const Error404 = require("./src/middleware/error404");
 const userRoute = require("./src/Routes/userRoute");
 const auth = require("./src/middleware/auth");
+const User = require("./src/Model/User");
 //middleware
 app.use(cors());
 app.use(express.json());
@@ -16,6 +17,19 @@ app.use("/api/v1/user", userRoute);
 //test auth
 app.get("/admin", [auth], (req, res) => {
   res.status(200).json(req.user);
+});
+
+//Get request for Users
+app.get("/api/v1/fetchUser", [auth], async (req, res) => {
+  const { _id } = req.user;
+  try {
+    const users = await User.findOne({ _id: _id });
+    const { Email, FirstName, LastName } = users;
+    res.status(200).json({ Email, FirstName, LastName });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Login before you proceed" });
+  }
 });
 
 app.use(Error404); // unavailable route
