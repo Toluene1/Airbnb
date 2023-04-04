8aa5ed08c4d2a4f56de31a773be44188fc065b2e
Toluene1 <adaramoti111@gmail.com>
2023-04-04 17:55:55 +0100

    added the LoginFooter component to HomePage

---
+++ client-side/src/components/LoginFooter/LoginFooter.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./LoginFooter.css";
+import { AiOutlineSearch, AiOutlineHeart } from "react-icons/ai";
+import { BiUserCircle, BiMessageAlt } from "react-icons/bi";
+import { FaAirbnb } from "react-icons/fa";
+
+function LoginFooter(params) {
+  return (
+    <section>
+      <div className="footerDivMain ">
+        <section>
+          <div className="footerDiv1 text-dark text-center ">
+            {/* <Link to={"/"} className="text-decoration-none "> */}
+            <div className=" divWithin ">
+              <AiOutlineSearch className="iconFooter" /> <br />
+              <span className="textFooter">Explore1</span>
+            </div>
+            {/* </Link> */}
+            <div className=" divWithin ">
+              <AiOutlineHeart className="iconFooter" /> <br />
+              <span className="textFooter">Wishlists</span>
+            </div>
+            <div className=" divWithin ">
+              <BiUserCircle className="iconFooter" /> <br />
+              <span className="textFooter">login</span>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="footerDiv2 text-dark text-center d-none">
+            {/* <Link to={"/"} className="text-decoration-none "> */}
+            <div className=" divWithin ">
+              <AiOutlineSearch className="iconFooter" /> <br />
+              <span className="textFooter">Explore</span>
+            </div>
+            {/* </Link> */}
+            <div className=" divWithin ">
+              <AiOutlineHeart className="iconFooter" /> <br />
+              <span className="textFooter">Wishlists</span>
+            </div>
+            <div className=" divWithin ">
+              <FaAirbnb className="iconFooter" /> <br />
+              <span className="textFooter">Trips</span>
+            </div>
+            <div className=" divWithin ">
+              <BiMessageAlt className="iconFooter" /> <br />
+              <span className="textFooter">Inbox</span>
+            </div>
+            <div className=" divWithin ">
+              <BiUserCircle className="iconFooter" /> <br />
+              <span className="textFooter">profile</span>
+            </div>
+          </div>
+        </section>
+      </div>
+    </section>
+  );
+}
+
+export default LoginFooter;
