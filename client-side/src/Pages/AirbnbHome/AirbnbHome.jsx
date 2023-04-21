a2b15fb52b5bd6af0a7a271c5c39efbe0b8eeeed
Toluene1 <adaramoti111@gmail.com>
2023-04-21 15:04:03 +0100

    added the footer and modified the AirBnbHome section

--- client-side/src/Pages/AirbnbHome/AirbnbHome.jsx
+++ client-side/src/Pages/AirbnbHome/AirbnbHome.jsx
@@ -3,10 +3,19 @@ import "./AirbnbHome.css";
 import Airbnblogo from "../../assets/airbnb-logo.png";
 import Airbnbsetup from "../../assets/airbnb-setup.webp";
 import Airbnbsetup2 from "../../assets/airbnbsetup2.webp";
+import divScroll1 from "../../assets/divScroll1.webp";
+import divScroll2 from "../../assets/divScroll2.webp";
+import divScroll3 from "../../assets/divSxroll3.webp";
 import { Link, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BiHomeCircle } from "react-icons/bi";
-import { Context } from "../../Provider/Context";
+import { FaRegCopyright } from "react-icons/fa";
+import { TbWorld } from "react-icons/tb";
+import {
+  AiFillFacebook,
+  AiOutlineInstagram,
+  AiFillTwitterSquare,
+} from "react-icons/ai";
 
 function AirbnbHome() {
   const [rangeval, setRangeval] = useState("85");
@@ -101,7 +110,145 @@ function AirbnbHome() {
           <img src={Airbnbsetup} className="airbnbSetup" alt="" />
           <img src={Airbnbsetup2} className="airbnbSetup2" alt="" />
         </div>
+        <div className="divTextCover gap-2">
+          <div className="divTexts">
+            <p className="fw-bold">One-to-one guidance from a Superhost</p>
+            <p style={{ marginTop: "-10px" }}>
+              We’ll match you with a Superhost in your area, who’ll guide you
+              from your first question to your first guest—by phone, video call,
+              or chat.
+            </p>
+          </div>
+          <div className="divTexts">
+            <p className="fw-bold">One-to-one guidance from a Superhost</p>
+            <p style={{ marginTop: "-10px" }}>
+              We’ll match you with a Superhost in your area, who’ll guide you
+              from your first question to your first guest—by phone, video call,
+              or chat.
+            </p>
+          </div>
+          <div className="divTexts">
+            <p className="fw-bold">One-to-one guidance from a Superhost</p>
+            <p style={{ marginTop: "-10px" }}>
+              We’ll match you with a Superhost in your area, who’ll guide you
+              from your first question to your first guest—by phone, video call,
+              or chat.
+            </p>
+          </div>
+        </div>
       </section>
+      <section>
+        <h2 className="airbnbText mt-5 ">
+          Introducing Airbnb-friendly apartments
+        </h2>
+        <div className="divFriendlyApart gap-2">
+          <div className="divScroll">
+            <img src={divScroll1} alt="" className="imageScroll" />
+            <div className="text-center mt-2">
+              <p className="fw-bold" style={{ fontSize: "12px" }}>
+                park-12 Apartments
+              </p>
+              <p style={{ fontSize: "12px", marginTop: "-18px" }}>
+                San Diego, Carlifonia
+              </p>
+            </div>
+          </div>
+          <div className="divScroll gap-2">
+            <img src={divScroll2} alt="" className="imageScroll" />
+            <div className="text-center mt-2">
+              <p className="fw-bold" style={{ fontSize: "12px" }}>
+                Old Town Apartments
+              </p>
+              <p style={{ fontSize: "12px", marginTop: "-18px" }}>
+                Scottsdale, Arizona
+              </p>
+            </div>
+          </div>
+          <div className="divScroll">
+            <img src={divScroll3} alt="" className="imageScroll" />
+            <div className="text-center mt-2">
+              <p className="fw-bold" style={{ fontSize: "12px" }}>
+                525 Olive Apartments
+              </p>
+              <p style={{ fontSize: "12px", marginTop: "-18px" }}>
+                San Diego, California
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <main className="footer">
+        <footer>
+          <div>
+            <h6>Support</h6>
+            <p>Help Center</p>
+            <p>Air cover</p>
+            <p>Supporting people with disabilities</p>
+            <p>Cancellation options</p>
+            <p>Our COVID-19 Response</p>
+            <p>Report a neighborhood concern</p>
+          </div>{" "}
+          <div>
+            <h6>Community</h6>
+            <p>Airbnb.org: disaster relief housing</p>
+            <p>Combating discrimination</p>
+          </div>{" "}
+          <div>
+            <h6>Hosting</h6>
+            <p>Airbnb your home</p>
+            <p>AirCover for Hosts</p>
+            <p>Explore hosting resources</p>
+            <p>Visit our community forum</p>
+            <p>How to host responsibly</p>
+            <p>Airbnb-friendly apartments</p>
+          </div>
+          <div>
+            <h6>Airbnb</h6>
+            <p>Learn about new features</p>
+            <p>AirCover for Hosts</p>
+            <p>Explore hosting resources</p>
+            <p>carrers</p>
+            <p>investors</p>
+            <p>Gift-card</p>
+          </div>
+        </footer>
+        <hr />
+
+        <main className="copyright">
+          <div>
+            <p>
+              {" "}
+              <FaRegCopyright />
+            </p>
+            <p>Airbnb,</p>
+            <p>Inc.</p>
+            <p> . </p>
+            <p>Terms . </p>
+            <p> site map . </p>
+            <p>Privacy . </p>
+            <p>Your Privacy choices</p>
+          </div>
+          <div className="icon">
+            <p>
+              <TbWorld />
+            </p>
+            <p>English (US) </p>
+            <p>$</p>
+            <p>USD</p>
+            <p>
+              {" "}
+              <AiFillFacebook className="fs-5" />{" "}
+            </p>
+            <p>
+              <AiOutlineInstagram className="text-dark fs-5" />
+            </p>
+            <p>
+              {" "}
+              <AiFillTwitterSquare className="fs-5" />
+            </p>
+          </div>
+        </main>
+      </main>
     </main>
   );
 }
