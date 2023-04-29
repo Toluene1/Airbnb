21683144040cf159dca6dd8070d358725814942d
Toluene1 <adaramoti111@gmail.com>
2023-04-29 19:54:25 +0100

    added the disable feature for the Next button, changed the state for the singleDivs

--- client-side/src/Pages/Photos/Photos.jsx
+++ client-side/src/Pages/Photos/Photos.jsx
@@ -7,112 +7,75 @@ import { Link } from "react-router-dom";
 import httpAuth from "../../Services/config";
 
 function Photos() {
-  const [photos, setPhotos] = useState(true);
+  const [photos, setPhotos] = useState(false);
   const [details, setDetails] = useState([]);
-
-  const showImages = () => {
-    setPhotos(false);
-  };
 
   const handleUploadImage = async (e) => {
     const data = new FormData();
     const file = e.target.files[0];
     data.append(e.target.id, file);
-    setDetails([
-      ...details,
-      { file_id: e.target.id, uploaded_file: e.target.files[0] },
-    ]);
+    setPhotos(true);
+    setDetails([...details, { file_id: e.target.id, uploaded_file: file }]);
     const creatImgSrc = await URL.createObjectURL(file);
     e.target.closest("div").nextElementSibling.src = creatImgSrc;
   };
 
-  //   const postPropertyImages = async () => {
-  //     try {
-  //       const response = await httpAuth.post("/user/updateProfile", profile);
-  //       setUser(response.data.user);
-  //       handleSaveUser(response.data.user);
-  //       seteditprofile(false);
-  //       setloading(false);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
   function handleSubmit(e) {
-    e.preventDefault();
+    // e.preventDefault();
     console.log(details);
   }
 
   return (
     <main>
       <PropertyNav />
-      <section className="px-3">
-        {photos ? (
-          <section className="sectionDiv animate__animated animate__fadeInRight">
-            <div className="texts">
-              <h3>Add some photos of your castle</h3>
-              <p className="fw-light" style={{ fontSize: "18px" }}>
-                You'll need 5 photos to get started. You can add more or make
-                changes later.
-              </p>
+      <section className="px-3 sectionDiv">
+        <section className=" mt-5 animate__animated animate__fadeInRight">
+          <div className="texts">
+            <h3>Add some photos of your castle</h3>
+            <p className="fw-light" style={{ fontSize: "18px" }}>
+              You'll need 5 photos to get started. You can add more or make
+              changes later.
+            </p>
+          </div>
+          <div className="texts2">
+            <div>
+              <span className=" reduceText">Choose at least 5 photos</span>
             </div>
-            <div className="photoBox">
-              <div>
-                <img
-                  src={photoDemo}
-                  style={{ width: "100px", height: "100px" }}
-                  alt=""
+            <div>
+              <div className="image-input text-center">
+                <label htmlFor="my-file">
+                  <div className="button-addMore">
+                    <BsPlus className="fs-3" />
+                    <span className="mt-5">Add more</span>
+                  </div>
+                </label>
+                <input type="file" id="my-file" name="image" />
+              </div>
+            </div>
+          </div>
+          {/* cover photo */}
+          <div className="photoBoxdiv mt-3">
+            <div className="singleDivsCover  position-relative ">
+              <div className="image-input text-center my-3 bg-danger position-absolute">
+                <label htmlFor="coverPhoto">
+                  <img
+                    src={photoDemo}
+                    style={{ width: "60px", height: "60px" }}
+                    alt=""
+                  />
+                </label>
+                <input
+                  type="file"
+                  id="coverPhoto"
+                  onChange={handleUploadImage}
                 />
-                <h5>Choose at least 5 photos</h5>
-                <div className="file-input text-center my-3">
-                  <label htmlFor="coverPhoto">Upload Photo</label>
-                  <input
-                    type="file"
-                    id="coverPhoto"
-                    name="image"
-                    onChange={showImages}
-                  />
-                </div>
               </div>
+              <img src="" alt="" style={{ width: "100%" }} />
             </div>
-          </section>
-        ) : (
-          <section>
-            <div className="texts2 mt-5">
-              <div className="mt-3">
-                <span className=" reduceText">Choose at least 5 photos</span>
-              </div>
-              <div>
-                <div className="image-input text-center my-3">
-                  <label htmlFor="my-file">
-                    <div className="button-addMore">
-                      <BsPlus className="fs-3" />
-                      <span className="mt-5">Add more</span>
-                    </div>
-                  </label>
-                  <input type="file" id="my-file" name="image" />
-                </div>
-              </div>
-            </div>
-            {/* cover photo */}
-            <div className="photoBoxdiv mt-3">
-              <div className="singleDivsCover  position-relative ">
-                <div className="image-input text-center my-3 bg-danger position-absolute">
-                  <label htmlFor="coverPhoto">
-                    <img
-                      src={photoDemo}
-                      style={{ width: "60px", height: "60px" }}
-                      alt=""
-                    />
-                  </label>
-                  <input
-                    type="file"
-                    id="coverPhoto"
-                    onChange={handleUploadImage}
-                  />
-                </div>
-                <img src="" alt="" style={{ width: "100%" }} />
-              </div>
-            </div>
+          </div>
+        </section>
+        {photos && (
+          <section className="">
             <div className="photoBoxDivs gap-3">
               {/* second  */}
               <div className="singleDivs  position-relative ">
@@ -237,11 +200,13 @@ function Photos() {
         <p className="text-decoration-underline fw-bold">
           <Link to={"/become-a-host/amenities"}>Back</Link>
         </p>
-        {/* <Link to={"/become-a-host/title"} className="text-white"> */}
-        <button className="Navfooterbtn" onClick={handleSubmit}>
+        <button
+          onClick={handleSubmit}
+          className={`${details.length >= 5 ? "Navfooterbtn" : "disabledbtn"}`}
+          disabled={details.length >= 5 ? false : true}
+        >
           Next
         </button>
-        {/* </Link> */}
       </footer>
     </main>
   );
