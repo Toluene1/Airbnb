e309dc2f28325679f99a72c5654d09a293beb8e4
Toluene1 <adaramoti111@gmail.com>
2023-05-03 00:07:19 +0100

    modified and added the property GetRoute to the Review page

--- client-side/src/Pages/Review/Review.jsx
+++ client-side/src/Pages/Review/Review.jsx
@@ -2,22 +2,27 @@ import PropertyNav from "../../components/PropertyNav/PropertyNav";
 import "./Review.css";
 import { Link, useNavigate } from "react-router-dom";
 import { BiNote } from "react-icons/bi";
-import { BsCalendar4Week, BsPen } from "react-icons/bs";
+import { BsCalendar4Week, BsPen, BsStarFill } from "react-icons/bs";
 import { useContext, useEffect, useState } from "react";
 import httpAuth from "../../Services/config";
 import { Context } from "../../Provider/Context";
 function Review() {
   const [property, setProperty] = useState({});
   const { propertyId } = useContext(Context);
+  const [loading, setloading] = useState(true);
+  const [img, setimg] = useState("");
   let isMounted = true;
   useEffect(() => {
     const fetchUser = async () => {
       try {
+        setloading(true);
         const response = await httpAuth.get(
           `property/findproperty/${propertyId}`
         );
         setProperty(response.data.prop);
+        setloading(false);
       } catch (error) {
+        setProperty({});
         console.log(error.response.data.msg);
       }
     };
@@ -29,25 +34,50 @@ function Review() {
       isMounted = false;
     };
   }, []);
-  console.log(property);
 
   return (
     <main>
       <PropertyNav />
-      <section className="mt-5 px-3">
+      <section className="mt-5 px-4" style={{ marginBottom: "100px" }}>
         <div className="texts">
           <h5 className="reduceText">Review your listing</h5>
           <p className="fw-light mt-3" style={{ fontSize: "18px" }}>
             Here's what we'll show to guests. Make sure everything looks good.
           </p>
         </div>
-        <div className="sectionsDiv gap-3">
-          <div className="getImages shadow">
-            <h1>Tolu</h1>
+        <div className="sectionsDiv gap-4 ">
+          <div className="getImages shadow px-3">
+            <div className="Images">
+              {loading ? (
+                <span className="spinner-border text-secondary"></span>
+              ) : (
+                <img
+                  src={property.images[0]}
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    borderRadius: "10px",
+                  }}
+                  alt=""
+                />
+              )}
+            </div>
+            <div className=" mt-3 flexDiv">
+              <div>
+                <h5>{property.About}</h5>
+                <p style={{ marginTop: "-8px" }}>
+                  <b>${property.price}</b>{" "}
+                  <span className="fw-light">night</span>
+                </p>
+              </div>
+              <div>
+                new <BsStarFill style={{ fontSize: "12px" }} />
+              </div>
+            </div>
           </div>
           <div className="getDetails">
             <h3>What's next?</h3>
-            <div className="d-flex mb-1">
+            <div className="d-flex mb-2 mt-3">
               <div>
                 {" "}
                 <BiNote className="fs-2 me-3" />
@@ -63,7 +93,7 @@ function Review() {
                 </p>
               </div>
             </div>
-            <div className="d-flex mb-1">
+            <div className="d-flex my-2">
               <div>
                 {" "}
                 <BsCalendar4Week className="fs-2 me-3" />
@@ -103,17 +133,7 @@ function Review() {
           <Link to={"/become-a-host/structure"}>Back</Link>
         </p>
 
-        {/* <button
-          disabled={isDisabled}
-          className={`${isDisabled ? "disabledbtn" : "Navfooterbtn"}`}
-          onClick={postPrivacy}
-        >
-          {loading ? (
-            <span className="spinner-border text-secondary"></span>
-          ) : (
-            "Next"
-          )}
-        </button> */}
+        <button className="Navfooterbtn">Next</button>
       </footer>
     </main>
   );
