d037399027c91f9670d3382e2c8dd6d7f6b5109c
Mekuzd <emekaseun.es@gmail.com>
2023-05-04 14:20:57 +0100

    added new route to property route

--- API/src/Routes/propertyRoute.js
+++ API/src/Routes/propertyRoute.js
@@ -6,6 +6,8 @@ const {
   updatePropertyLocation,
   findProperty,
   getAllProperty,
+  getHostProperty,
+  findPropertynoAuth,
 } = require("../Controllers/propertyController");
 const auth = require("../middleware/auth");
 const router = Router();
@@ -14,5 +16,7 @@ router.post("/updateproperty/:id", [auth], updateProperty);
 router.post("/updatepropertylocation/:id", [auth], updatePropertyLocation);
 router.post("/uploadimages/:id", [auth], uploadPropertyImages);
 router.get("/findproperty/:id", [auth], findProperty);
+router.get("/findproperty/:id", findPropertynoAuth);
+router.get("/findhostproperty/", [auth], getHostProperty);
 router.get("/getallproperty/", getAllProperty);
 module.exports = router;
