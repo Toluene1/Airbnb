db031bb397119fcdf3c99742065023395c748d8d
emeka oluwaseun <emekaseun.es@gmail.com>
2023-05-06 12:20:36 +0100

    renamed the individual property route doest require authorization

--- API/src/Routes/propertyRoute.js
+++ API/src/Routes/propertyRoute.js
@@ -16,7 +16,7 @@ router.post("/updateproperty/:id", [auth], updateProperty);
 router.post("/updatepropertylocation/:id", [auth], updatePropertyLocation);
 router.post("/uploadimages/:id", [auth], uploadPropertyImages);
 router.get("/findproperty/:id", [auth], findProperty);
-router.get("/findproperty/:id", findPropertynoAuth);
+router.get("/property/:id", findPropertynoAuth);
 router.get("/findhostproperty/", [auth], getHostProperty);
 router.get("/getallproperty/", getAllProperty);
 module.exports = router;
