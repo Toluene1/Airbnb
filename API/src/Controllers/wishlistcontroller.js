d2ee462310a576dba7f7ccf3b10d4a1a65b70c5d
emeka oluwaseun <emekaseun.es@gmail.com>
2023-05-10 16:22:39 +0100

    added new route delete wislist

--- API/src/Controllers/wishlistcontroller.js
+++ API/src/Controllers/wishlistcontroller.js
@@ -32,5 +32,20 @@ const getWishlist = async (req, res) => {
     res.status(500).json({ msg: "please contact the admin " });
   }
 };
+const deleteWishlist = async (req, res) => {
+  const { _id } = req.user;
+  const { id } = req.params;
+  try {
+    await Wishlist.findOneAndRemove({
+      _id: id,
+      user: _id,
+    });
+    const wishlist = await Wishlist.find({ user: _id }).populate("property");
+    res.status(200).json({ wish: wishlist });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "please contact the admin " });
+  }
+};
 
-module.exports = { createWishlist, getWishlist };
+module.exports = { createWishlist, getWishlist, deleteWishlist };
--- API/src/Routes/wishlistRoute.js
+++ API/src/Routes/wishlistRoute.js
@@ -2,9 +2,11 @@ const { Router } = require("express");
 const {
   createWishlist,
   getWishlist,
+  deleteWishlist,
 } = require("../Controllers/wishlistcontroller");
 const router = Router();
 router.post("/create", createWishlist);
 router.get("/", getWishlist);
+router.delete("/:id", deleteWishlist);
 
 module.exports = router;
