12d785d5eff13ea88e843d70ce8ee75c76700f9c
Toluene1 <adaramoti111@gmail.com>
2023-05-11 19:04:46 +0100

    modified Listing Page

--- client-side/src/Pages/Listings/Listings.jsx
+++ client-side/src/Pages/Listings/Listings.jsx
@@ -5,17 +5,19 @@ import "./Listings.css";
 import ListingsNav from "../../components/ListingsNav/ListingsNav";
 import { BiPlus } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import LoginFooter from "../../components/LoginFooter/LoginFooter";
 
 function Listings() {
-  const [property, setProperty] = useState({});
+  const [listings, setListings] = useState({});
+  const [loading, setLoading] = useState(false);
   let isMounted = true;
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        //    setloading(true);
+        setLoading(true);
         const response = await httpAuth.get(`property/findhostproperty`);
-        setProperty(response.data.prop);
-        //    setloading(false);
+        setListings(response.data.prop);
+        setLoading(false);
       } catch (error) {
         setProperty({});
         console.log(error.response.data.msg);
@@ -32,47 +34,62 @@ function Listings() {
   return (
     <main>
       <ListingsNav />
-      <section className="px-4">
-        <div className="firstSection">
-          <div>
-            <h2>{property?.length} listings</h2>
-          </div>
-          <div>
-            <Link to={"/become-a-host/overview"}>
-              <button className="createButton">
-                <BiPlus className="fs-5" />
-                <span className="ms-2">Create Listing</span>
-              </button>
-            </Link>
-            <Link to={"/become-a-host/overview"}>
-              <button className="plusButton">
-                <BiPlus className="fs-4" />
-              </button>
-            </Link>
-          </div>
+      {loading ? (
+        <div className="center-screen">
+          <span className="spinner-border text-danger"></span>
         </div>
-      </section>
-      <section className="property">
-        {property.length > 0 ? (
-          <>
-            <ul>
-              {property.map((property) => (
-                <li>
-                  {" "}
-                  <h3>{property?.structure} </h3>
-                </li>
-              ))}
-            </ul>
-          </>
-        ) : (
-          <div className="">
-            {" "}
-            <h6 className="text-danger">
-              properties of the selected category cant be found
-            </h6>
-          </div>
-        )}
-      </section>
+      ) : (
+        <main>
+          <section className="px-4">
+            <div className="firstSection">
+              <div>
+                <h2>{listings?.length} listings</h2>
+              </div>
+              <div>
+                <Link to={"/become-a-host/overview"}>
+                  <button className="createButton">
+                    <BiPlus className="fs-5" />
+                    <span className="ms-2">Create Listing</span>
+                  </button>
+                </Link>
+                <Link to={"/become-a-host/overview"}>
+                  <button className="plusButton">
+                    <BiPlus className="fs-4" />
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </section>
+          <section>
+            <section className="Listings mb-5">
+              {listings.length > 0 ? (
+                <article>
+                  {listings.map((listings, index) => (
+                    <Link to={`/property/${listings?._id}`} key={index}>
+                      <main className="shadow">
+                        <div>
+                          <img src={listings.images[0]} alt="" />
+                        </div>
+
+                        {/* <button onClick={(e) => deleteWishlist(e, wish._id)}>
+                      {" "}
+                      <AiOutlineDelete />
+                    </button> */}
+                      </main>
+                    </Link>
+                  ))}
+                </article>
+              ) : (
+                <h5 className=" text-secondary">
+                  No property has been added to wish list
+                </h5>
+              )}
+            </section>
+          </section>
+        </main>
+      )}
+
+      <LoginFooter />
     </main>
   );
 }
