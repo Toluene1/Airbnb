e8b93d6de50800c64e9e862896628643804b95f4
Toluene1 <adaramoti111@gmail.com>
2023-05-19 00:22:25 +0100

    modified the checkout and checkin components

--- client-side/src/utils/SearchDrops/CheckInDates.jsx
+++ client-side/src/utils/SearchDrops/CheckInDates.jsx
@@ -1,11 +1,22 @@
 import "./SearchDrops.css";
+import calendar from "../../../src/assets/calendar.png";
 
 function CheckInDates({ toggle }) {
   return (
     <main>
       <section
-        className={` ${toggle == 1 ? "mainDiv shadow" : "tabcontent"}`}
-      ></section>
+        className={` ${
+          toggle == 1 ? "mainDiv shadow bg-primary" : "tabcontent"
+        }`}
+      >
+        <div>
+          <img
+            src={calendar}
+            alt=""
+            style={{ height: "100%", width: "700px", borderRadius: "20px" }}
+          />
+        </div>
+      </section>
     </main>
   );
 }
--- client-side/src/utils/SearchDrops/CheckOutDate.jsx
+++ client-side/src/utils/SearchDrops/CheckOutDate.jsx
@@ -1,11 +1,18 @@
 import "./SearchDrops.css";
+import checkout from "../../../src/assets/checkout.png";
 
 function CheckOutDates({ toggle }) {
   return (
     <main>
-      <section
-        className={` ${toggle == 2 ? "mainDiv shadow" : "tabcontent"}`}
-      ></section>
+      <section className={` ${toggle == 2 ? "mainDiv shadow" : "tabcontent"}`}>
+        <div>
+          <img
+            src={checkout}
+            alt=""
+            style={{ height: "100%", width: "700px", borderRadius: "20px" }}
+          />
+        </div>
+      </section>
     </main>
   );
 }
