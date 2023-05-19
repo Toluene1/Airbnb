b89f90c8925a564c8936e403e850d5afbf9ddb3c
Toluene1 <adaramoti111@gmail.com>
2023-05-19 11:58:24 +0100

    added the HomepageFooter component to utils Page, modified the searchDrop components

---
+++ client-side/src/utils/HomepageFooter.jsx
@@ -0,0 +1,30 @@
+import "./Searchdrops/SearchDrops.css";
+import { TbWorld } from "react-icons/tb";
+import { BiCopyright } from "react-icons/bi";
+
+function HomepageFooter() {
+  return (
+    <main className=" footerDiv bg-light px-5 py-2 shadow fixed-bottom">
+      <div className="d-flex gap-3">
+        <div>
+          {" "}
+          <BiCopyright className="me-1 adjustMargin" /> 2023 Airbnb, Inc{" "}
+        </div>
+        <div>Terms</div>
+        <div>Sitemap</div>
+        <div>Privacy</div>
+        <div>your privacy choices</div>
+      </div>
+      <div className="d-flex gap-3" style={{ flexWrap: "no-wrap" }}>
+        <div>
+          {" "}
+          <TbWorld className="me-1 adjustMargin" /> English (Us)
+        </div>
+        <div>$ USD</div>
+        <div>support and resources</div>
+      </div>
+    </main>
+  );
+}
+
+export default HomepageFooter;
