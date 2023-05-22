879d7dc07d893bb3f7212568b812d51b395b5814
mekuzd <101795048+mekuzd@users.noreply.github.com>
2023-05-23 00:32:18 +0100

    Update index.js

--- API/index.js
+++ API/index.js
@@ -6,38 +6,38 @@ const { PORT, MONGO_URL } = require("./src/Config/config");
 const userRouter = require("./src/Routes/userRoute");
 const propertyRouter = require("./src/Routes/propertyRoute");
  const categoryRouter = require("./src/Routes/categoryRoute")
-// const wishlistRouter = require("./src/Routes/wishlistRoute");
-// const auth = require("./src/middleware/auth");
+const wishlistRouter = require("./src/Routes/wishlistRoute");
+const auth = require("./src/middleware/auth");
 
-// security packages
-// const helmet = require("helmet");
-// const xss = require("xss-clean");
-// const cors = require("cors");
+security packages
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
 
-//middleware
-// app.use(
-//   cors({ origin: ["http://localhost:4000", "https://airbnbr34.vercel.app"] }),
-// );
+middleware
+app.use(
+  cors({ origin: ["http://localhost:4000", "https://airbnbr34.vercel.app"] }),
+);
 
-// app.use(helmet());
-// app.use(xss());
-//  app.use(express.json());
+app.use(helmet());
+app.use(xss());
+ app.use(express.json());
 
 // //Routes
 app.get("/", (req, res) => {
   res.send("hello");
 });
-// app.use("/api/v1/user", userRouter);
-// app.use("/api/v1/property", propertyRouter);
-// app.use("/api/v1/category", categoryRouter);
-// app.use("/api/v1/wishlist", [auth], wishlistRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/property", propertyRouter);
+app.use("/api/v1/category", categoryRouter);
+app.use("/api/v1/wishlist", [auth], wishlistRouter);
 
-// app.use(Error404); // unavailable route
+app.use(Error404); // unavailable route
 
-// // connect to db and listen to port
+ // connect to db and listen to port
 const start = async () => {
   try { 
-//     await mongoose.connect(MONGO_URL);
+   await mongoose.connect(MONGO_URL);
     app.listen(PORT);
   } catch (error) {
     console.log(error);
