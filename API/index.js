1190264babfa7120ef81661141807763799d352a
mekuzd <101795048+mekuzd@users.noreply.github.com>
2023-05-22 16:25:45 +0100

    Update index.js

--- API/index.js
+++ API/index.js
@@ -1,49 +1,49 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
-const { PORT, MONGO_URL } = require("./src/Config/config");
-const Error404 = require("./src/middleware/error404");
-const userRouter = require("./src/Routes/userRoute");
-const propertyRouter = require("./src/Routes/propertyRoute");
-const categoryRouter = require("./src/Routes/categoryRoute");
-const wishlistRouter = require("./src/Routes/wishlistRoute");
-const auth = require("./src/middleware/auth");
+// const mongoose = require("mongoose");
+// const { PORT, MONGO_URL } = require("./src/Config/config");
+// const Error404 = require("./src/middleware/error404");
+// const userRouter = require("./src/Routes/userRoute");
+// const propertyRouter = require("./src/Routes/propertyRoute");
+// const categoryRouter = require("./src/Routes/categoryRoute");
+// const wishlistRouter = require("./src/Routes/wishlistRoute");
+// const auth = require("./src/middleware/auth");
 
 // security packages
-const helmet = require("helmet");
-const xss = require("xss-clean");
-const cors = require("cors");
+// const helmet = require("helmet");
+// const xss = require("xss-clean");
+// const cors = require("cors");
 
 //middleware
-app.use(
-  cors({ origin: ["http://localhost:4000", "https://airbnbr34.vercel.app"] }),
-);
+// app.use(
+//   cors({ origin: ["http://localhost:4000", "https://airbnbr34.vercel.app"] }),
+// );
 
-app.use(helmet());
-app.use(xss());
-app.use(express.json());
+// app.use(helmet());
+// app.use(xss());
+// app.use(express.json());
 
-//Routes
+// //Routes
 app.get("/", (req, res) => {
   res.send("hello");
 });
-app.use("/api/v1/user", userRouter);
-app.use("/api/v1/property", propertyRouter);
-app.use("/api/v1/category", categoryRouter);
-app.use("/api/v1/wishlist", [auth], wishlistRouter);
+// app.use("/api/v1/user", userRouter);
+// app.use("/api/v1/property", propertyRouter);
+// app.use("/api/v1/category", categoryRouter);
+// app.use("/api/v1/wishlist", [auth], wishlistRouter);
 
-app.use(Error404); // unavailable route
+// app.use(Error404); // unavailable route
 
-// connect to db and listen to port
-const start = async () => {
-  try {
-    await mongoose.connect(MONGO_URL);
-    app.listen(PORT, () =>
-      console.log(`Server is listening on port ${PORT}...`),
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
+// // connect to db and listen to port
+// const start = async () => {
+//   try {
+//     await mongoose.connect(MONGO_URL);
+//     app.listen(PORT, () =>
+//       console.log(`Server is listening on port ${PORT}...`),
+//     );
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-start();
+// start();
