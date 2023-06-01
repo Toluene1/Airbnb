76028a38e17aecfebb69ab43b456879917885652
unknown <adaramoti111@gmail.com>
2023-06-01 15:18:23 +0100

    modified the error404 page, added the footerProp

--- client-side/src/Pages/error404.jsx
+++ client-side/src/Pages/error404.jsx
@@ -1,10 +1,51 @@
 import Navbar from "../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
+import FooterProp from "../components/FooterProp.jsx/FooterProp";
 
 const Error404 = () => {
   return (
     <main>
       <Navbar />
-      <h1>no available page</h1>
+      <section className="errorDivMain mx-auto mt-5 px-3">
+        <div className="errorDiv1">
+          <p className="fs-3 fw-bold">
+            We can’t seem to find the page you’re looking for
+          </p>
+          <p className="mt-4">here are some helpful links instead</p>
+          <ul>
+            <Link to={"/"}>
+              <li className="text-decoration-underline fw-bold fs-5 py-2">
+                home
+              </li>
+            </Link>
+            <Link to={"/"}>
+              <li className="text-decoration-underline fw-bold fs-5  py-2">
+                Travelling on Airbnb
+              </li>
+            </Link>
+            <Link to={"/AirbnbHome"}>
+              <li className="text-decoration-underline fw-bold fs-5   py-2">
+                Hosting on Airbnb
+              </li>
+            </Link>
+            <Link to={"/"}>
+              <li className="text-decoration-underline fw-bold fs-5   py-2">
+                Trust and safety
+              </li>
+            </Link>
+            <Link to={"/"}>
+              <li className="text-decoration-underline fw-bold fs-5  py-2">
+                Sitemap
+              </li>
+            </Link>
+          </ul>
+        </div>
+
+        <div className="errorDiv2">
+          <h1>Tolu</h1>
+        </div>
+      </section>
+      <FooterProp />
     </main>
   );
 };
