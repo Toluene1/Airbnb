06928a14c53f2c543015dcad78284474b609fe0e
Toluene1 <adaramoti111@gmail.com>
2023-06-06 09:53:19 +0100

    added the SearchProperties component

---
+++ client-side/src/components/SearchProperties.jsx
@@ -0,0 +1,43 @@
+import Modal from "react-bootstrap/Modal";
+import "./Navbar/Navbar.css";
+import FilterBody from "./FilterBody";
+import { MdCancel } from "react-icons/md";
+import { useContext } from "react";
+import { Context } from "../Provider/Context";
+
+function SearchProperties(props) {
+  const { setFilterShow, setFullscreen, setSearchShow } = useContext(Context);
+  function hideFilter() {
+    setSearchShow(false);
+    setFullscreen(false);
+  }
+  return (
+    <>
+      <div id="modalDisplay">
+        <Modal
+          {...props}
+          className="modalStyle pt-2"
+          style={{ zIndex: "5000" }}
+        >
+          <Modal.Header className="p-1 pe-3 py-2 ">
+            <div className="divCoverHeader">
+              <div>
+                <button className="modalButtonHeader1" onClick={hideFilter}>
+                  <MdCancel className="fs-3 text-dark" />
+                </button>
+              </div>
+              <div>
+                <span className="fw-bold">Full Preview</span>
+              </div>
+            </div>
+          </Modal.Header>
+          <Modal.Body className="ms-2">
+            <FilterBody hideFilter={hideFilter} />
+          </Modal.Body>
+        </Modal>
+      </div>
+    </>
+  );
+}
+
+export default SearchProperties;
